d66c3265d3a4fa7b041ed95111bc9b9837999b17
ivan <ivansyn2013@yandex.ru>
2022-12-02 00:59:11 +0300

    Lesson3 done

--- mainapp/views.py
+++ mainapp/views.py
@@ -20,7 +20,6 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = 'mainapp/news.html'
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['news_title'] = 'Новость №'
@@ -30,7 +29,10 @@ class NewsPageView(TemplateView):
         context['range'] = range(5)
         return context
 
-
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self,page, **kwargs):
+        context = super().get_context_data(page=page,**kwargs)
+        return context
 
 class CoursesPageView(TemplateView):
     template_name = 'mainapp/courses_list.html'
