77823d99bd40b152cb3e0e88514e70f5138abff8
iv_note <ivan_note@local.ru>
2022-12-13 20:03:57 +0100

    admin someChanges3

--- authapp/admin.py
+++ authapp/admin.py
@@ -6,4 +6,4 @@ from authapp import models
 @admin.register(models.CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ["id", "username", "email", "is_active", "date_joined"]
-    ordering = ["-date_joined"] 
+    ordering = ["-date_joined"]
